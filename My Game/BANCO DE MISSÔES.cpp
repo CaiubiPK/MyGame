91a2f43e9b0edd933ca426c6edaa6ec598d9f3a6
Caiubi Aguiar Coutinho Lima <105654929+CaiubiPK@users.noreply.github.com>
2023-02-28 10:53:48 -0300

    Add files via upload

---
+++ My Game/BANCO DE MISSÔES.cpp
@@ -0,0 +1,29 @@
+
+
+
+
+
+class Missao
+{
+private:
+	string nome,objetivo
+	int moedas,xp,reputacao;
+public:
+	string getnome()
+	{
+		return this->nome;
+	}
+	string getnome()
+	{
+		return this->nome;
+	}
+	CompletarMissao()
+	{
+	}
+};
+
+
+
+
+missoes[1].setnome("");
+missoes[1].setobjetivo("");
